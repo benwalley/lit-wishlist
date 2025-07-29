ebda4cec0f03bc1075db558f6e39584f8ed99bbe
Ben Walley <ben.walley@classyllama.com>
2025-07-28 20:17:58 -0500

    fix api url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const envVars = {
     API_URL: import.meta.env.DEV
         ? `http://localhost:3000/api`  // Development
-        : `https://node-wishlist-website-1d9acdcb5bdb.herokuapp.com/api/'`,  // Production
+        : `https://node-wishlist-website-1d9acdcb5bdb.herokuapp.com/api`,  // Production
     STORAGE_ITEM_NAME: 'wishlist',
 }
